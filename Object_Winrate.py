6f81673b2abd241cb87d56195e6fd90c765056d9
NextrPlue <p.plue1881@gmail.com>
2023-09-11 21:28:16 +0900

    Update Object_Winrate.py

--- Object_Winrate.py
+++ Object_Winrate.py
@@ -83,7 +83,7 @@ def main() :
                                     'type':['average', 'average', 'first_object', 'first_object'],
                                     'win_rate':[League_Object.loc[select_team]['result'], League_Object.loc[select_team]['result'], 
                                                 League_Object.loc[select_team]['firstdragon_win'], League_Object.loc[select_team]['firstherald_win']]})
-        fig = plt.figure(figsize=(10, 4.5))
+        fig = plt.figure(figsize=(10, 4.7))
         sb.barplot(x='object', y='win_rate', data=FirstObj_Win, hue='type')
         st.pyplot(fig)
 
@@ -111,5 +111,5 @@ def main() :
         st.pyplot(fig)
 
         # 그래프 분석
-
+        
 main()
