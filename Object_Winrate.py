f99206d91a65c5db4931b10407cce6b551d8ddea
NextrPlue <98487008+NextrPlue@users.noreply.github.com>
2023-09-11 14:21:22 +0900

    Update Object_Winrate.py

--- Object_Winrate.py
+++ Object_Winrate.py
@@ -52,7 +52,6 @@ sb.barplot(x='object', y='win_rate', data=FirstObj_Win, hue='type')
 st.pyplot(fig)
 
 # 첫 용과 승률 산점도 그래프 그리기
-fig = plt.figure(figsize=(10, 4))
 sb.lmplot(x="firstdragon", y="result", data=League_Object, line_kws={'color' : 'red'})
 highlight_x = League_Object.loc[option, 'firstdragon']
 highlight_y = League_Object.loc[option, 'result']
