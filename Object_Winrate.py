0ddaa0115d356004bd3f3040f90f658140bd22d3
NextrPlue <p.plue1881@gmail.com>
2023-09-12 01:19:22 +0900

    Update Object_Winrate.py

--- Object_Winrate.py
+++ Object_Winrate.py
@@ -170,8 +170,10 @@ def main() :
             Buff_Win = pd.DataFrame({'object':buff_object,
                                         'win_rate':[League_Object.loc[select_team][(i+'_win')] for i in buff_object]})
             fig = plt.figure(figsize=(10, 4.7))
+            ax = fig.add_subplot()
             sb.barplot(x='object', y='win_rate', data=Buff_Win, label=select_team)
-            plt.axhline(y=League_Object.loc[select_team]['result'], xmin=0, xmax=1, color='red', linestyle='solid', label='mean')
+            ax.axhline(League_Object.loc[select_team]['result'], color='red', linestyle='solid', label='mean')
+            ax.text(0, League_Object.loc[select_team]['result']+10, 'Mean of winning rate')
             st.pyplot(fig)
             win_rate_list = [League_Object.loc[select_team]['infernal_win'], League_Object.loc[select_team]['mountain_win'], League_Object.loc[select_team]['cloud_win'], League_Object.loc[select_team]['ocean_win'], League_Object.loc[select_team]['chemtech_win'], League_Object.loc[select_team]['hextech_win']]
             buff = ['화염', '대지', '바람', '바다', '화학공학', '마법공학']
