4cd7b6141b0448d7dbc73c0b346729a27c9eb105
NextrPlue <p.plue1881@gmail.com>
2023-09-11 20:40:03 +0900

    Update Object_Winrate.py

--- Object_Winrate.py
+++ Object_Winrate.py
@@ -34,13 +34,13 @@ def selectYear(year_select="2023") :
     else :
         League = pd.read_csv('2023_LoL_esports_match_data_from_OraclesElixir.csv')
 
-def dataProcessing(league_select="모든 리그", checkbox_btn=False) :
+def dataProcessing(league_select="모든 리그") :
     global League, League_Object
     League = League[League['datacompleteness'] == 'complete']
     League = League[League['position'] == 'team']
     League = League[['teamname', 'league', 'result', 'firstdragon', 'firstherald', 'dragons', 'heralds', 'barons']]
     League['dragon_buff'] = (League['dragons'] >= 4.0) * 1
-    if checkbox_btn :
+    if league_select != "모든 리그" :
         League = League[League['league'] == league_select]
     League_Object = League.groupby('teamname').agg({'result':'mean'}).sort_values('result')
     League_Object['count'] = League.groupby('teamname').agg({'result':'count'})
@@ -68,13 +68,9 @@ with con1 :
 st.sidebar.title('🎮데이터 선택하기')
 select_year = st.sidebar.selectbox('분석할 년도를 선택하세요.', ['2014', '2015', '2016', '2017', '2018', '2019', '2020', '2021', '2022', '2023'])
 selectYear(select_year)
-checkbox_btn = st.sidebar.checkbox('선택한 리그 내에서 분석하기')
-if checkbox_btn :
-    league_list = League['league'].unique()
-else :
-    league_list = np.append(["모든 리그"], League['league'].unique())
+league_list = np.append(["모든 리그"], League['league'].unique())
 select_league = st.sidebar.selectbox('분석할 리그를 선택하세요.', league_list)
-dataProcessing(select_league, checkbox_btn)
+dataProcessing(select_league)
 if select_league == "모든 리그" :
     team_list = League
 else : 
