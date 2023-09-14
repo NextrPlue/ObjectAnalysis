9ce65a5a62acbc8f158c2cbf2f7cef4da78ce736
NextrPlue <p.plue1881@gmail.com>
2023-09-14 18:59:18 +0900

    Update Object_Winrate.py

--- Object_Winrate.py
+++ Object_Winrate.py
@@ -153,8 +153,8 @@ min_match = st.sidebar.slider('필요한 최소 경기 수를 선택하세요.',
 select_team2 = st.sidebar.selectbox('분석할 팀2을 선택하세요.', sorted(League_Predict['teamname'].unique().astype(str)))
 team1_result, team2_result, accuracy = predictWinner(select_team, select_team2)
 col1, col2, col3 = st.columns(3)
-col1.metric(select_team, team1_result, team1_result - team2_result)
-col2.metric(select_team2, team2_result, team2_result - team1_result)
+col1.metric(select_team, team1_result[0], team1_result[0] - team2_result[0])
+col2.metric(select_team2, team2_result[0], team2_result[0] - team1_result[0])
 col3.metric("Accuracy", accuracy)
 
 def main() :
